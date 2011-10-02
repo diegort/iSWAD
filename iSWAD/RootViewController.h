2e39f23889857a35cba12ee77f6fb9a7597e912f
Diego Montesinos <dmontesinos@MacBook-Pro-de-Diego-Montesinos.local>
2011-10-03 00:33:37 +0200

    Initial Commit

---
+++ iSWAD/RootViewController.h
@@ -0,0 +1,13 @@
+//
+//  RootViewController.h
+//  iSWAD
+//
+//  Created by Diego Montesinos on 03/10/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootViewController : UITableViewController
+
+@end
---
+++ iSWAD/iSWADAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  iSWADAppDelegate.h
+//  iSWAD
+//
+//  Created by Diego Montesinos on 03/10/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface iSWADAppDelegate : NSObject <UIApplicationDelegate>
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@property (nonatomic, retain) IBOutlet UINavigationController *navigationController;
+
+@end
